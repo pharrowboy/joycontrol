cf68c2351a1af4697010ab14ecd264b3f3d4f0ad
pharrowboy <msantore22@gmail.com>
2022-01-21 00:23:52 -0700

    Update controller_state.py

--- joycontrol/controller_state.py
+++ joycontrol/controller_state.py
@@ -206,14 +206,18 @@ async def button_press(controller_state, *buttons):
     await controller_state.send()
     
     
-async def button_update(controller_state, button, value):
-    button_state = controller_state.button_state
+async def stick_update(controller_state, stick, values):
+    if stick == 'l_stick_analog':
+        stick_state = controller_state.l_stick_state
+    elif stick == 'r_stick_analog':
+        stick_state = controller_state.r_stick_state        
 
-    # update button state
-    button_state.set_button(button, pushed=value)
+    # update stick state
+    stick_state.set_h(values['h'])
+    stick_state.set_v(values['v'])    
 
     # send report
-    await controller_state.send()  
+    await controller_state.send()
 
 async def button_release(controller_state, *buttons):
     """
